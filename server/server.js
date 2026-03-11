d6b2f69b48a57db2cfaa090e7ad1fcb2c8e9a7d7
SanmithDevadiga <sanmith@collectndeal.com>
2026-03-11 12:33:14 +0530

    check

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://data-beta-six.vercel.app"
   ],
   credentials: true
 }));

2fe3ec8639183d5afb5b5e153ff6e98b706f7266
SanmithD <sanmithdevadiga7@gmail.com>
2026-03-14 11:49:21 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -21,6 +21,12 @@ app.use(cors({
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+app.get('/', (req, res) => {
+  res.send({
+    message: "Hello world"
+  })
+})
+
 app.use("/api", cardRoutes);
 
 const PORT = process.env.PORT;
